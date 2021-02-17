986588195a3a8d0b2423f227368bc4f0a79b81b1
Soumy Jain <soumyjain14@gmail.com>
2021-02-18 02:24:11 +0530

    OOPS2

---
+++ ObjectOrientedProgramming/src/vehicle/Vehicle.java
@@ -0,0 +1,11 @@
+package vehicle;
+
+public class Vehicle {
+	protected String color;
+	int maxSpeed;
+	
+	public void print() {
+		System.out.println("vehicle color :"+color);
+		System.out.println("vehicle speed :"+maxSpeed);
+	}
+}
---
+++ ObjectOrientedProgramming/src/vehicle/VehicleUse.java
@@ -0,0 +1,13 @@
+package vehicle;
+
+ class VehicleUse {
+	 
+	 public static void main(String[] args) {
+	 Vehicle v=new Vehicle();
+	 v.color="black";
+	 v.maxSpeed=100;
+	 v.print();
+	 
+	
+	 }
+}
