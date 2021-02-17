0452e93d5b4b8806db4dfdc7c94d3d502fe76b27
Soumy Jain <soumyjain14@gmail.com>
2021-02-17 18:48:34 +0530

    Polynomial Solved

--- ObjectOrientedProgramming/src/classes_and_objects/PolynomialUse.java
+++ ObjectOrientedProgramming/src/classes_and_objects/PolynomialUse.java
@@ -5,17 +5,20 @@ public class PolynomialUse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Polynomial p1=new Polynomial();
-		p1.setCoefficient(0, 3);
-		p1.setCoefficient(2, 2);
-		
-		p1.print();
+		p1.setCoefficient(3,5);
+		p1.setCoefficient(2,3);
+		p1.setCoefficient(2, 7);
 		Polynomial p2=new Polynomial();
-		p2.setCoefficient(1,5);
-		p2.setCoefficient(2, 3);
+		p2.setCoefficient(0,7);
+		p2.setCoefficient(1, 8);
+		p1.print();
 		p2.print();
-		Polynomial a=p1.multiply(p2);
-		a.print();
-
+//		Polynomial1 a=p1.add(p2);
+//		a.print1();
+		p1.subtract(p2);
+		p1.print();
+//		Polynomial1 a=p1.multiply(p2);
+//		a.print1();
 		
 		
 	}
