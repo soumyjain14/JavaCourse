d4b8906d99afdb43b4f599ce3a3e3ed685d13f69
Soumy Jain <soumyjain14@gmail.com>
2021-02-20 12:53:41 +0530

    Recursion notes

--- Recursion/src/recursion/Power.java
+++ Recursion/src/recursion/Power.java
@@ -1,10 +1,28 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Power {
+	static int hero;
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int fib(int n) {
+		if(n==0){
+			return 0;
+		}
+		if(n==1 || n==2) {
+			return 1;
+		}
+		int smallAns = fib(n-1)+fib(n-2);
+		return smallAns;
 	}
 
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int ans=fib(n);
+		System.out.println(ans);
+		
+	}
 }
