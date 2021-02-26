06b233bd28842ceb2a7e09c33ab03d71ba3bce08
Soumy Jain <soumyjain14@gmail.com>
2021-02-26 15:52:05 +0530

    Return Sequences

---
+++ Recursion/src/recursion/ReturnSubsequences.java
@@ -0,0 +1,37 @@
+package recursion;
+
+public class ReturnSubsequences {
+	
+	public static String[] returnSubsequences(String str) {
+		if(str.length()==0) {
+			String ans[]= {" "};
+			return ans;
+			
+		}
+		
+		String smallAns[]=returnSubsequences(str.substring(1));
+		String ans[]=new String[smallAns.length*2];
+		for(int i=0;i<smallAns.length;i++) {
+			ans[i]=smallAns[i];
+		}
+		for(int i=0;i<smallAns.length;i++) {
+			ans[i+smallAns.length]=str.charAt(0)+smallAns[i];
+		}
+		return ans;
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="xyz";
+		String ans[]=returnSubsequences(str);
+		for(int i=0;i<ans.length;i++) {
+			System.out.println(ans[i]);
+		}
+		
+
+	}
+
+}
