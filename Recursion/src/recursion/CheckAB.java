0d32b83762f6c7c67e28850761d606e7bcf96c63
Soumy Jain <soumyjain14@gmail.com>
2021-03-02 18:33:45 +0530

    Check AB Question

--- Recursion/src/recursion/CheckAB.java
+++ Recursion/src/recursion/CheckAB.java
@@ -3,17 +3,23 @@ package recursion;
 public class CheckAB {
 	
 	public static boolean checkAB(String input) {
-		if(input.length()<=0) {
+		if(input.length()==0) {
 			return true;
 		}
-		if(input.length()==1 && input.charAt(0)=='b') {
-			return false;
+		
+		if(input.length()==1) {
+			if(input.charAt(0)=='a') {
+				return true;
+			}else {
+				return false;
+			}
 		}
-		
-		
-		
-		boolean smallAns=checkAB(input.substring(1));
-		return false;
+		if(input.length()>=3 && input.substring(0, 3)=="abb") {
+			return checkAB(input.substring(3));
+			
+		}else {
+			return checkAB(input.substring(1));
+		}
 		
 	}
 
