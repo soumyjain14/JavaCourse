9d093769de147e72898635fde95d4e6b6b45d311
Soumy Jain <soumyjain14@gmail.com>
2021-03-10 00:25:45 +0530

    Unique elements in Array

--- Recursion/src/recursion/UniqueElements.java
+++ Recursion/src/recursion/UniqueElements.java
@@ -1,26 +1,20 @@
 package recursion;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class RoughWork {
+public class UniqueElements {
 
 	public static int findUnique(int[] arr) {
 		int ans=0;
-		
 		for(int i=0;i<arr.length;i++) {
-			ans=arr[i];
-			if(ans==1) {
-				return arr[i];
-			}
-			
+			ans=ans^arr[i];
+
 		}
-		return -1;
+		return ans;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {0,7,2,5,4,7,1,3,6};
-		int ans=findUnique(arr);
+		int input[]= {2,3,1,1,3,6,2};
+		int ans=findUnique(input);
 		System.out.println(ans);
 
 	}
