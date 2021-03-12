442e7fe1a92d779a38b1cb633af0648a95467ed9
Soumy Jain <soumyjain14@gmail.com>
2021-03-13 03:01:48 +0530

    Array List

---
+++ LinkedList/src/arrayList/ArrayListDemo.java
@@ -0,0 +1,36 @@
+package arrayList;
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> list= new ArrayList<Integer>();
+		System.out.println(list.size());
+		list.add(10);
+		list.add(15);
+		list.add(25);
+		for(int i=0;i<list.size();i++) {
+			System.out.print(list.get(i)+ " ");
+		}
+		System.out.println();
+		list.add(0, 50);
+		
+		for(int i=0;i<list.size();i++) {
+			System.out.print(list.get(i)+ " ");
+		}
+		System.out.println();
+		
+		list.remove(2);
+		list.set(0, 75);
+		list.isEmpty();
+		for(int element : list) {
+			System.out.print(element + " ");
+		}
+		
+		
+
+	}
+
+}
