a93c45eb7cc29975f521ca48f5872f69ee2bbe03
Soumy Jain <soumyjain14@gmail.com>
2021-03-29 17:58:24 +0530

    Rough

--- Recursion/src/recursion/RoughWork.java
+++ Recursion/src/recursion/RoughWork.java
@@ -1,20 +1,27 @@
 package recursion;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
+import java.lang.Math;
 
 public class RoughWork {
 	
-	
-	
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="110";
-		int k=1;
-//		boolean ans=hasAllCodes(s,k);
-		System.out.println("hi");
-//		
+		Scanner scan = new Scanner(System.in);
+		int t=scan.nextInt();
+		while(t>0) {
+		int v=scan.nextInt();
+		int u=scan.nextInt();
+		int a=scan.nextInt();
+		int s=scan.nextInt();
+		if(v==u || Math.sqrt(((u*u)-(2*a*s)))<=v) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}		
+		t--;
+		}
 		
 	}
 
