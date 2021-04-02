660678558cfd2afb70373aac72e267d9b3bf524b
Soumy Jain <soumyjain14@gmail.com>
2021-04-02 15:28:18 +0530

    Alternate Nodes

---
+++ LinkedList/src/linkedList/AlternateNodes.java
@@ -0,0 +1,55 @@
+package linkedList;
+
+import java.util.Scanner;
+
+public class AlternateNodes {
+	
+	public static Node<Integer> takeInput(){
+		Scanner scan=new Scanner(System.in);
+		Node<Integer>head=null;
+		Node<Integer>tail=null;
+		int data=scan.nextInt();
+		while(data!=-1) {
+			Node<Integer> newNode=new Node<>(data);
+			if(head==null) {
+				head=newNode;
+				tail=newNode;
+			}else {
+				tail.next=newNode;
+				tail=newNode;
+			}
+			data=scan.nextInt();
+		}
+		return head;
+		
+	
+	}
+	
+	public static void printLL(Node<Integer> head) {
+		Node<Integer> temp=head;
+		while(temp!=null) {
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+		}
+	}
+	
+	public static void deleteAlternateNodes(Node<Integer> head) {
+		Node<Integer> temp=head;
+		while(temp!=null && temp.next!=null) {
+			temp.next=temp.next.next;
+			
+			temp=temp.next;
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Node<Integer>head=takeInput();
+		deleteAlternateNodes(head);
+		printLL(head);
+		
+
+	}
+
+}
