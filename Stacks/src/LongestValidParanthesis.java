bcc37e8ae7fd140102cfbe30927dccc6816cd05e
Soumy Jain <soumyjain14@gmail.com>
2021-04-05 11:34:36 +0530

    Longest Valid Paranthesis

--- Stacks/src/LongestValidParanthesis.java
+++ Stacks/src/LongestValidParanthesis.java
@@ -2,42 +2,34 @@ import java.util.Stack;
 public class LongestValidParanthesis {
 	
 	public static int lvp(String exp) {
-		Stack<Character> stack=new Stack<>();
-		int max=-1;int count=0;
+		Stack<Integer> stack=new Stack<>();
+		stack.push(-1);
+		int result=0;
 		for(int i=0;i<exp.length();i++) {
-			char x=exp.charAt(i);			
-			if(x=='(' && stack.size()==0) {
-				stack.push(x);
-			}
-			if(stack.isEmpty() && x==')') {
-				stack.push(x);
-			}
-			char a;
-			if(stack.size()>0 && x==')') {
-				a=stack.peek();
-				if(a==')') {
-					stack.push(x);
-				}else {
-					count+=2;
+			char x=exp.charAt(i);
+			
+			if(x=='(') {
+				stack.push(i);
+			}else {
+				if(!stack.isEmpty()) {
 					stack.pop();
-					
+				}
+				
+				if(!stack.isEmpty()) {
+					result=Math.max(result, i-stack.peek());
+				}else {
+					stack.push(i);
 				}
 			}
-			if(!stack.isEmpty()) {
-				count-=1;
-			}
 		}
-		
-		return count;
-		
-        
+		return result;
     }
 		
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s="()((()";
+		String s="(())";
 		int ans=lvp(s);
 		System.out.println(ans);
 		
