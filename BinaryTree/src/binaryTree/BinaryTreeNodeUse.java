29690acc83f02fa5cbd01fe9fe81285f29e30f4f
Soumy Jain <soumyjain14@gmail.com>
2021-04-15 14:19:50 +0530

    Binary Tree Node Print Function

--- BinaryTree/src/binaryTree/BinaryTreeNodeUse.java
+++ BinaryTree/src/binaryTree/BinaryTreeNodeUse.java
@@ -1,9 +1,10 @@
 package binaryTree;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class BinaryTreeNodeUse {
-	
+
 	public static void printTree(BinaryTreeNode<Integer> root) {
 		if(root==null) {
 			return;
@@ -19,7 +20,7 @@ public class BinaryTreeNodeUse {
 		printTree(root.left);
 		printTree(root.right);
 	}
-	
+
 	public static BinaryTreeNode<Integer> takeInput(Scanner scan){
 		System.out.println("Enter the root data");
 		int rootData=scan.nextInt();
@@ -31,14 +32,87 @@ public class BinaryTreeNodeUse {
 		rootNode.right=takeInput(scan);
 		return rootNode;
 	}
+
+	public static BinaryTreeNode<Integer> takeInputLevelWise(){
+		Scanner scan=new Scanner(System.in);
+		LinkedList<BinaryTreeNode<Integer>> queue=new LinkedList<>();
+		System.out.println("Enter the root data");
+		int rootData=scan.nextInt();
+		BinaryTreeNode<Integer> rootNode=new BinaryTreeNode<Integer>(rootData);
+		queue.add(rootNode);
+		while(!queue.isEmpty()) {
+			BinaryTreeNode<Integer> frontNode=queue.poll();
+			if(frontNode==null) {
+				return null;
+			}
+			BinaryTreeNode<Integer> leftNode=null;
+			BinaryTreeNode<Integer> rightNode=null;
+			System.out.println("enter the left data for "+frontNode.data);
+			int leftData=scan.nextInt();
+			if(leftData!=-1) {
+				leftNode=new BinaryTreeNode<Integer>(leftData);
+			}
+			frontNode.left=leftNode;
+			if(leftNode!=null) {
+				queue.add(leftNode);
+			}
+			System.out.println("enter the right data for "+frontNode.data);
+			int rightData=scan.nextInt();
+			if(rightData!=-1) {
+				rightNode=new BinaryTreeNode<Integer>(rightData);
+			}			
+			frontNode.right=rightNode;
+			if(rightNode!=null) {
+				queue.add(rightNode);
+			}
+
+
+		}
+
+		return rootNode;
+	}
+
+	public static void printTreeBetter(BinaryTreeNode<Integer> root) {
+		if(root==null) {
+			return;
+		}
+
+		LinkedList<BinaryTreeNode<Integer>> queue=new LinkedList<>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			
+			BinaryTreeNode<Integer> frontNode=queue.poll();
+			String toBePrinted="";
+			
+				toBePrinted=frontNode.data+":";	
+				if(frontNode.left!=null) {
+				toBePrinted+="L:"+frontNode.left.data+",";
+				queue.add(frontNode.left);
+				}else {
+					toBePrinted+="L:"+"-1"+",";
+					//queue.poll();
+				}
+				if(frontNode.right!=null) {
+				toBePrinted+="R:"+frontNode.right.data+",";
+				queue.add(frontNode.right);	
+				}else {
+					toBePrinted+="R:"+"-1"+",";
+					//queue.poll();
+				}			
+				
+					System.out.println(toBePrinted);
 	
-	
+		}
+	}
+
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan=new Scanner(System.in);
-		BinaryTreeNode<Integer> root=takeInput(scan);
-		printTree(root);
-		
+		//Scanner scan=new Scanner(System.in);
+		BinaryTreeNode<Integer> root=takeInputLevelWise();
+		printTreeBetter(root);
+
 
 	}
 
